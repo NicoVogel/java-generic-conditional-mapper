8db8ff5e32b42a4643ad1928d16b17646d55d396
Nico Vogel <nico-vogel97@outlook.de>
2021-02-15 23:25:02 +0000

    allow chaining of mappings

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,11 +3,9 @@ package com.mycompany.app;
 public class App {
     public static void main(String[] args) {
         var a = new SomeClass();
-        var b = new AnotherClass();
-
-        var mapper = new Mapper<>(a, b);
-        mapper.mapNotNull(SomeClass::getNumber, AnotherClass::setInteger);
-        mapper.mapNotNull(SomeClass::getNumbers, AnotherClass::setList);
+        var b = new Mapper<>(a, new AnotherClass()).mapNotNull(SomeClass::getNumber, AnotherClass::setInteger)
+                .mapNotNull(SomeClass::getNumbers, AnotherClass::setList)
+                .mapNotEmpty(SomeClass::getNumbers, AnotherClass::setList).get();
 
         System.out.println(b.value);
         System.out.println(b.test);
--- src/main/java/com/mycompany/app/Mapper.java
+++ src/main/java/com/mycompany/app/Mapper.java
@@ -1,26 +1,46 @@
 package com.mycompany.app;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class Mapper<FROM, TO> {
-    FROM from;
-    TO to;
+    private FROM from;
+    private TO to;
 
-    public <VALUE_TYPE> void mapNotNull(Function<? super FROM, ? extends VALUE_TYPE> getter,
-            BiConsumer<? super TO, VALUE_TYPE> setter) {
-        mapCondition(getter, setter, Objects::nonNull);
+    public TO get() {
+        return this.to;
     }
 
-    public <VALUE_TYPE> void mapCondition(Function<? super FROM, ? extends VALUE_TYPE> getter,
-            BiConsumer<? super TO, VALUE_TYPE> setter, Function<VALUE_TYPE, Boolean> condition) {
+    public <VALUE_TYPE> Mapper<FROM, TO> mapCondition(Function<? super FROM, ? extends VALUE_TYPE> getter,
+            BiConsumer<? super TO, VALUE_TYPE> setter, Predicate<VALUE_TYPE> predicate) {
         VALUE_TYPE value = getter.apply(from);
-        if (condition.apply(value)) {
+        if (predicate.test(value)) {
             setter.accept(to, value);
         }
+        return this;
+    }
+
+    public <VALUE_TYPE> Mapper<FROM, TO> mapNotNull(Function<? super FROM, ? extends VALUE_TYPE> getter,
+            BiConsumer<? super TO, VALUE_TYPE> setter) {
+        return mapCondition(getter, setter, Objects::nonNull);
+    }
+
+    public <VALUE_TYPE extends List<?>> Mapper<FROM, TO> mapNotEmpty(
+            Function<? super FROM, ? extends VALUE_TYPE> getter, BiConsumer<? super TO, VALUE_TYPE> setter) {
+        return mapCondition(getter, setter, andPredicates(Objects::nonNull, List::isEmpty));
+    }
+
+    @SafeVarargs
+    public static <R> Predicate<R> andPredicates(Predicate<R>... predicates) {
+        return Arrays.stream(predicates).reduce(Predicate::and).orElse(x -> true);
     }
 }
